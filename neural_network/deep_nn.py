5ecf59928a984a6faf292f74087ec58290b53cd3
Magomed Makhmudov <mmriadovich@gmail.com>
2018-07-11 23:46:00 +0300

    deep_nn

--- neural_network/deep_nn.py
+++ neural_network/deep_nn.py
@@ -12,17 +12,38 @@ class Network(object):
         self.arch = arch
         self.weight = [np.random.randn(y,x) for x,y in zip(arch[:-1], arch[1:])]
 
+        self.activations = []
+        self.layer_error = []
+
 
     def feedforward(self, a):
         for w in self.weight:
             a = sigmoid(np.dot(w,a))
+            self.activations.append(a)
         return a
 
     def train(self, a, y):
-        pass
+        self.activations.clear()
+        self.feedforward(a)
+
+        output_error = y - self.activations[-1]
+        error = output_error
+        for w in reversed(self.weight):
+            error = np.dot(w.T, error)
+            self.layer_error.append(error)
+        self.layer_error.reverse()
+        self.layer_error.append(output_error)
+
+        
 
 
 if __name__ == '__main__':
-    nn = Network([2, 3, 2])
-    print(nn.weight)
-    print(nn.feedforward(np.array([1,1], ndmin=2).T))
+    nn = Network([2, 2, 1])
+    for a in nn.activations:
+        print(a)
+
+    x = np.array([1,1], ndmin=2).T
+    y = np.array([1], ndmin=2).T
+    nn.train(x,y)
+    for i in nn.layer_error:
+        print(i)
